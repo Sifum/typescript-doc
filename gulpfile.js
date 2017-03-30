a93d68f32326097447f2df529a222fdbc01caca9
pocanda <472799073@qq.com>
2017-03-30 11:13:42 +0800

    3.30 SECOND

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const ts = require("gulp-typescript");
 const tsProject = ts.createProject("tsconfig.json");
 const gutil = require("gulp-util");
 const sass = require("gulp-ruby-sass");
+const del = require("del");
 const buffer = require('vinyl-buffer');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const $ = gulpLoadPlugins();
@@ -42,26 +43,31 @@ gulp.task('scripts', ()=>{
             .pipe(reload({stream: true}));
 });
 
+gulp.task('fonts', () => {
+  return gulp.src('app/fonts/*.*')
+    .pipe(gulp.dest('.tmp/fonts'))
+    .pipe(gulp.dest('dist/fonts'));
+});
 
 
 gulp.task('html', ['styles', 'scripts'], ()=>{
     return gulp.src('app/*.html')
             .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
             .pipe($.if('*.js', $.uglify()))
-            //.pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
-            //.pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+            .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
+            .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
             .pipe(gulp.dest('dist'));
 });
 
-gulp.task('images', ()=>{
-    return gulp.src('app/images/**/*')
-            // .pipe($.cache($.imagemin({
-            //     progressive: true,
-            //     interlaced: true,
-            //     svgoPlugins: [{cleanupIDs: false}]
-            // })))
-            .pipe(gulp.dest('dist/images'));
-});
+// gulp.task('images', ()=>{
+//     return gulp.src('app/images/**/*')
+//             .pipe($.cache($.imagemin({
+//                 progressive: true,
+//                 interlaced: true,
+//                 svgoPlugins: [{cleanupIDs: false}]
+//             })))
+//             .pipe(gulp.dest('dist/images'));
+// });
 
 
 gulp.task('extras', ()=>{
@@ -74,10 +80,41 @@ gulp.task('extras', ()=>{
 });
 
 
-// gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 
+gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      routes: {
+        '/bower_components': 'bower_components',
+        './scripts' : '.tmp/scripts'
+      }
+    }
+  });
 
+  gulp.watch([
+    'app/*.html',
+    'app/images/**/*',
+    '.tmp/fonts/**/*'
+  ]).on('change', reload);
+
+  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/scripts/**/*.ts', ['scripts']);
+  gulp.watch('app/fonts/**/*', ['fonts']);
+  gulp.watch('bower.json', ['wiredep', 'fonts']);
+});
+
+gulp.task('wiredep', () => {
+  gulp.src('app/styles/*.scss')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/styles'));
+});
 
 
 
